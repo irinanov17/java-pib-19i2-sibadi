cdd34093bb677636badb1f2ea4bced42488f426b
Irina <irina.nov17@mail.ru>
2021-11-24 14:04:46 +0600

    Exe4

---
+++ Restoran/Rest/src/model/Menu.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    public List<Dish> dishes;
+
+    public Menu() {
+        dishes = new ArrayList<>();
+    }
+}
